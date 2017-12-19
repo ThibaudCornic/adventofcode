87560e328722c55c1c6db80956365e3487cb397b
Thibaud Cornic <thibaud_cornic@sigmadesigns.com>
2017-12-19 16:23:36 +0100

    Add second part of day 15

--- day15.c
+++ day15.c
@@ -13,11 +13,28 @@ int main()
 	int i = 0;
 	int matches = 0;
 
-	while (i < 40000000) {
-		gena = (gena * 16807) % 2147483647;
-		genb = (genb * 48271) % 2147483647;
-		if ((gena & 0xffff) == (genb & 0xffff))
+	while (i < 5000000) {
+		int stopa = 0;
+		int stopb = 0;
+		uint16_t a = 0;
+		uint16_t b = 0;
+		while(stopa == 0) {
+			gena = (gena * 16807) % 2147483647;
+			if (!(gena & 0x3)) {
+				a = (uint16_t)gena;
+				stopa = 1;
+			}
+		}
+		while(stopb == 0) {
+			genb = (genb * 48271) % 2147483647;
+			if (!(genb & 0x7)) {
+				b = (uint16_t)genb;
+				stopb = 1;
+			}
+		}
+		if (a == b) {
 			matches++;
+		}
 		i++;
 	}
 	printf("A is %lu, B is %lu, matches = %d\n", gena, genb, matches);
